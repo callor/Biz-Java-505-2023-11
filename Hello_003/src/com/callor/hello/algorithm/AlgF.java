48d637abd9aa19f30a7bfb6f158763cb18c7fc21
callor <callor@callor.com>
2023-11-23 16:49:15 +0900

    과제

--- Hello_003/src/com/callor/hello/algorithm/AlgF.java
+++ Hello_003/src/com/callor/hello/algorithm/AlgF.java
@@ -1,5 +1,18 @@
 package com.callor.hello.algorithm;
 
 public class AlgF {
+	public static void main(String[] args) {
+		
+		/*
+		 * 우리나라 화폐는
+		 * 5만, 1만, 5천, 1천, 5백, 1백, 50, 10원 단위이다
+		 * 아래 pay 에 저장된 값에서
+		 * 각각 화폐단위만큼 몇개씩이 포함되는지 
+		 * 출력하시오
+		 */
+		int pay = 3587960;
+		
+		
+	}
 
 }
