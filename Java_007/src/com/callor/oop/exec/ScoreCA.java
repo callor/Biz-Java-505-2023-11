7f582a5ba921b6588000a37dd70274fabca03971
callor <callor@callor.com>
2023-12-04 12:59:04 +0900

    점수계산

--- Java_007/src/com/callor/oop/exec/ScoreCA.java
+++ Java_007/src/com/callor/oop/exec/ScoreCA.java
@@ -11,6 +11,15 @@ public class ScoreCA {
 		for(int i = 0 ; i < scores.length ; i++) {
 			scores[i] = new ScoreService();
 		}
+		
+		// 10개의 배열의 각 과목점수를 Random으로 생성하고자 한다
+		for(int i  = 0 ; i < scores.length; i++) {
+			scores[i].scoreKor = (int)(Math.random() * 50) + 51;
+			scores[i].scoreEng = (int)(Math.random() * 50) + 51;
+			scores[i].scoreMath = (int)(Math.random() * 50) + 51;
+		}
+		
+		// 배열에 저장된 점수 화면에 출력하기
 				
 		
 	}
