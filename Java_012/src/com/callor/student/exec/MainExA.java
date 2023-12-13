a1c37a825ad0a2044f588d76210366d23d6fd338
callor <callor@callor.com>
2023-12-13 11:51:25 +0900

    student

--- Java_012/src/com/callor/student/exec/MainExA.java
+++ Java_012/src/com/callor/student/exec/MainExA.java
@@ -1,5 +1,17 @@
 package com.callor.student.exec;
 
+import com.callor.student.service.StartService;
+import com.callor.student.service.impl.StartServiceImplV2;
+
 public class MainExA {
 
+	public static void main(String[] args) {
+		StartService start = new StartServiceImplV2();
+		// start.mainMenu();
+		// Integer result = start.selectMenu();
+		// System.out.println(result);
+		start.startApp();
+	}
+	
+	
 }
--- Java_012/src/com/callor/student/service/impl/StartServiceImplV2.java
+++ Java_012/src/com/callor/student/service/impl/StartServiceImplV2.java
@@ -19,8 +19,57 @@ public class StartServiceImplV2 extends StartServiceImplV1 {
 		System.out.println("업무를 선택해 주세요");
 		Line.sLine(50);
 		for(MenuIndex menu : MenuIndex.values()) {
-			System.out.printf("%d. %s\n",menu.getIndex(), menu);
+			String item = menu.toString();
+			item = item.replace("_", " ");
+			System.out.printf("%d. %s\n",menu.getIndex(), item);
 		}
 		System.out.println("QUIT. 종료");
+		Line.sLine(50);
+	} // end mainMenu()
+	
+	
+	@Override
+	public Integer selectMenu() {
+		Integer selectMenu = 0;
+		while(true) {
+			this.mainMenu();
+			System.out.print("업무 선택 >> ");
+			String str = scan.nextLine();
+			if(str.equals("QUIT")) return null;
+			
+			try {
+				selectMenu = Integer.valueOf(str);
+			} catch (Exception e) {
+				System.out.println("업무선택은 정수값을 입력하세요");
+				continue;
+			}
+			if(selectMenu < 1 
+					|| selectMenu > MenuIndex.values().length) {
+				System.out.printf("업무 선택은 1 ~ %d 범위의 값입니다\n",
+						MenuIndex.values().length );
+				continue;
+			}
+			return selectMenu;
+		}
+	} // end selectMenu
+	
+	@Override
+	public void startApp() {
+		while(true) {
+			Integer selectMenu = this.selectMenu();
+			if(selectMenu == null) break;
+			else if(selectMenu == 1) {
+				System.out.println("학생정보 입력");
+			} else if(selectMenu == 2) {
+				System.out.println("학생정보 조회");
+			} else if(selectMenu == 3) {
+				System.out.println("학생정보 가져오기");
+			} else if(selectMenu == 4) {
+				System.out.println("학생정보 출력");
+			}
+		}
+		System.out.println("업무종료~!!");
+		
 	}
+	
 }
